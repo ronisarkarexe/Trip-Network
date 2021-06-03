af132733701220330fda777c0bbd54efb919d902
Aomi Sarkar <ronisarkar2244@gmail.com>
2021-06-03 11:58:59 +0600

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Roni c Sarkar </Text>
+      <Text>Joni Biswas</Text>
       <StatusBar style="auto" />
     </View>
   );
